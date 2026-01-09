052e4d08f6bafca81e785af836958d960d8b8ca7
AnnaKulai79 <hannahku68@gmail.com>
2026-01-09 17:33:53 +0100

    read db

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
+import os
+import db_sql as db
+from dotenv import load_dotenv
+load_dotenv()
 
-engine = create_engine('postgresql://postgres:2625217@localhost:5432/postgres')
 
 query_category = """
 WITH orders_by_month AS (
@@ -86,68 +89,17 @@ def clean_marketing_data(df_raw: pd.DataFrame) -> pd.DataFrame:
     return clean
 
 
-#  EDA
-# print(df.info())
-# print(f"Розмір таблиці: {df.shape}")
-# print("Пропуски по колонках:")
-# print(df.isna().sum())
-# print(f"Кількість дублікатів: {df.duplicated().sum()}")
-# print(df.describe())
-# print(df.describe(include='object'))
-# print(df.nunique())
 
-# Clean Database
-df = clean_marketing_data(load_marketing_csv(csv_path))
-# print(df)
+def main():
+    csv_path = "marketing_spend.csv"
+    df = clean_marketing_data(load_marketing_csv(csv_path))
+    print(df)
+    
+    orders_sql_path = "orders.sql"
+    pg_url = os.getenv("POSTGRES_URL")
+    pg_engine = db.get_postgres_engine(pg_url)
+    orders = db.load_orders_postgres(pg_engine)
+    print(orders)
 
-
-# df = df.drop_duplicates()
-# df = df.dropna(subset=['channel', 'spend_amount'])
-# df['spend_amount'] = pd.to_numeric(df['spend_amount'], errors='coerce')
-# # print(df['spend_amount'].isna().sum(), "рядків мають некоректні символи")
-# df = df[df['spend_amount'] >= 0]
-# # print(df.shape)
-# # print(df)
-
-# df['channel'] = df['channel'].str.lower().str.strip()
-# df = df.groupby(['month', 'channel'], as_index=False)['spend_amount'].sum()
-# df['month'] = pd.to_datetime(df['month'])
-# pivot_df = df.pivot(index='month', columns='channel', values='spend_amount')
-# # print("Пропущено записів за місяцями:")
-# # print(pivot_df.isna().sum())
-
-
-
-
-# Завантажуємо в DataFrame
-df_orders = pd.read_sql(query, engine)
-
-# print(df_orders)
-# print(df)
-
-df_orders['month'] = pd.to_datetime(df_orders['month'], utc=True).dt.tz_localize(None).dt.normalize()
-df['month'] = pd.to_datetime(df['month'], utc=True).dt.tz_localize(None).dt.normalize()
-df_merged = pd.merge(df, df_orders, on='month', how='left')
-# print(df_merged)
-# print(df_merged['total_sales'].max(), df_merged['spend_amount'].max())
-
-df_plot = df_merged.copy().sort_values('month') 
-df_plot['month_str'] = df_plot['month'].dt.strftime('%Y-%m-%d')
-
-max_val = max(df_plot['total_sales'].max(), df_plot['spend_amount'].max())
-
-upper_limit = max_val * 1.15
-fig, ax1 = plt.subplots(figsize=(14, 7))
-
-sns.barplot(data=df_plot, x='month_str', y='total_sales', ax=ax1, color='skyblue', alpha=0.4, label='Total Sales', errorbar=None) 
-ax2 = ax1.twinx() 
-sns.lineplot(data=df_plot, x='month_str', y='spend_amount', hue='channel', marker='o', ax=ax2)
-ax1.set_ylim(0, upper_limit) 
-ax2.set_ylim(0, upper_limit)
-
-ax2.legend(loc='upper left', bbox_to_anchor=(1, 1))
-
-ax1.set_xticklabels(ax1.get_xticklabels(), rotation=45) 
-plt.title('Sales vs Marketing Spend (Correct 1:1 Scale)') 
-plt.tight_layout() 
-plt.show()
+if __name__ == "__main__":
+    main()
